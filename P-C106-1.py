df71017ff300bd047e8deac621c806ba9b273c91
FatehjotSingh <72024514+FatehjotSingh@users.noreply.github.com>
2021-07-16 20:02:11 +0530

    Add files via upload

---
+++ P-C106-1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import plotly.express as py
+import pandas as pd
+import csv
+
+def plot():
+    fil= open('csvs/3.csv')
+    reader=csv.DictReader(fil)   
+    chart= py.scatter(reader,x='Coffee',y='sleep',size_max=50,title='Sleep-Coffee')
+    chart.show()
+
+def datasource():
+    cf=[]
+    sh=[]
+    fil= open('csvs/3.csv')
+    reader=csv.DictReader(fil)
+    for i in reader:
+        cf.append(float(i['Coffee']))
+        sh.append(float(i['sleep']))
+    return {'x':cf, 'y':sh}
+
+def findcorr(source):
+    correlation= np.corrcoef(source['x'],source['y'])
+    print(correlation[0,1])
+
+def setup():
+    src= datasource()
+    findcorr(src)
+    plot()
+
+setup();
---
+++ P-C106-2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import plotly.express as py
+import pandas as pd
+import csv
+
+def plot():
+    fil= open('csvs/4.csv')
+    reader=csv.DictReader(fil)   
+    chart= py.scatter(reader,x='Days',y='Marks',size_max=50,title='Attendance-Percentage ratio')
+    chart.show()
+
+def datasource():
+    marks=[]
+    days=[]
+    fil= open('csvs/4.csv')
+    reader=csv.DictReader(fil)
+    for i in reader:
+        marks.append(float(i['Marks']))
+        days.append(float(i['Days']))
+    return {'x':days, 'y':marks}
+
+def findcorr(source):
+    correlation= np.corrcoef(source['x'],source['y'])
+    print(correlation[0,1])
+
+def setup():
+    src= datasource()
+    findcorr(src)
+    plot()
+
+setup();
